3bdf5f5b7332e0c12a03df200201374f64b81b0c
colorfultrees <colorfultrees@users.noreply.github.com>
2023-03-06 20:56:55 +0100

    home section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { HeaderComponent } from './header/header.component';
+import { SectionHomeComponent } from './section-home/section-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionHomeComponent
   ],
   imports: [
     BrowserModule,
